2df8dc339c2e050456bd7200e46a3746b52f6bad
Pradipta Basu <pradiptabasu@users.noreply.github.com>
2019-08-28 00:48:08 +0530

    Updates

---
+++ precache-manifest.12e83890fcb8913646f1bbe34c3dc8b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c45001e8a6c4f4ad5cc007d15865238",
+    "url": "/pbasu-resume/index.html"
+  },
+  {
+    "revision": "0ec8d4edf0c0e12877c2",
+    "url": "/pbasu-resume/static/css/main.4d045730.chunk.css"
+  },
+  {
+    "revision": "31f7dad48eafe193d465",
+    "url": "/pbasu-resume/static/js/2.2a8fa528.chunk.js"
+  },
+  {
+    "revision": "0ec8d4edf0c0e12877c2",
+    "url": "/pbasu-resume/static/js/main.0d834a62.chunk.js"
+  },
+  {
+    "revision": "0d14ff848ded11da07e8",
+    "url": "/pbasu-resume/static/js/runtime~main.7846b3fe.js"
+  },
+  {
+    "revision": "053a870091dc68a3a28372346a42f889",
+    "url": "/pbasu-resume/static/media/Pradipta Basu - Detailed.053a8700.pdf"
+  },
+  {
+    "revision": "a5d2cf873298161e8be55bc94d49acc6",
+    "url": "/pbasu-resume/static/media/Pradipta Basu.a5d2cf87.pdf"
+  }
+]);
